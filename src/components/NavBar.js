df991b0a28feac3787ab9769687e5cf88368266b
MORGANJASON <420morganjason@gmail.com>
2023-04-25 19:10:21 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{
+  
+  <nav className="nav">
+        <a href='/' className='a-nav'>
+            <ul>
+                <li> <NavLink to='/movies'>movies</NavLink></li>
+                 <li> <NavLink to='/directors'>directors </NavLink></li>
+                  <li> <NavLink to='/actors'>actors</NavLink></li>
+
+            </ul>
+        </a>
+
+
+
+      </nav>}</div>;
 }
 
 export default NavBar;
